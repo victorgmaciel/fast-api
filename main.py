a5a22d5fe4322054b4c93ce5902395749f7ede28
Victor! <victor88@uw.edu>
2023-04-18 16:01:14 -0600

    changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from typing import Optional,List
-from fastapi import FastAPI 
+from typing import Optional, List
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
 
 app = FastAPI()
 
 users = []
+
 
 class User(BaseModel):
     email: str
@@ -17,7 +18,14 @@ class User(BaseModel):
 async def get_users():
     return users
 
+
 @app.post("/users")
 async def create_user(user: User):
-    users.append(user) 
+    users.append(user)
     return "Success"
+
+
+@app.get("/users/{id}")
+async def get_user(id: int = Path(..., description="The ID of the user you want to retrieve", gt=2),
+                   q: str = Query(None, max_length=5)):
+    return {"user": users[id], "query": q}
